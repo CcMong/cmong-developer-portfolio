fab349fa02d1d01cbd1bf746626e53057889700d
CcMong <fullstackcodex@gmail.com>
2023-03-31 12:59:41 +0100

    installed Sanity.

---
+++ backend_sanity/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'r49zk1xs',
+    dataset: 'production'
+  }
+})
---
+++ steps.js
@@ -0,0 +1,8 @@
+/*
+Want to create a Fullstack portfolio site using React and Sanity
+- Create a new repo on Github, and clone
+- Install React on local repo, and push the changes to Github
+- Deploy on Netlify via Github, and create access token to ensure automatic deployment using a push.yml file
+- Create a folder in the root directory for the backend part. Install sanity on this directory
+- After the installation, look inside the backend folder for the schema folder, and then index.js. Create another file, eg. testimonials.js
+*/
