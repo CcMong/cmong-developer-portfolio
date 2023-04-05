9251a16ad8b2f1fb2ca552ad3c71cc2347b06c25
CcMong <fullstackcodex@gmail.com>
2023-04-05 11:42:47 +0100

    Navbar has now been created, both for larger and smaller screens. The latter was done using Framer Motion slide-out.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,29 +1,63 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import "./Navbar.scss";
-import logo from "../../assets/images/cmong-dev-logo.jpg"
+import logo from "../../assets/images/cmong-dev-logo.jpg";
+import { HiMenuAlt4, HiX } from "react-icons/hi";
+import { easeOut, motion } from "framer-motion";
 
 const Navbar = () => {
-  return (
-    <nav className="app__navbar">
-        <div className="app__navbar-logo">
-            <img src={logo} alt="logo" />
-        </div>
-        <ul className="app__navbar-links">
-            {["Home", "About", "Skills", "Portfolio", "Contact"].map((element) => (
+    
+    const [toggle, setToggle] = useState(false);
+
+    return (
+        <nav className="app__navbar">
+            <div className="app__navbar-logo">
+                <img src={logo} alt="logo" />
+            </div>
+            <ul className="app__navbar-links">
+                {["Home", "About", "Skills", "Portfolio", "Contact"].map((element) => (
                 <li className="app__flex p-text" style={{listStyleType: "none"}}key={`link-${element}`}>
                     <div />
                     <a href={`#${element}`}>{element}</a>
                 </li>
-            ))}
-        </ul>
-        {/*We need to account for mobile devices by having a hamburger menu which pops out when we click on it at smaller screen widths*/}
-        <div className="app__navbar-menu">
+                ))}
+            </ul>
+            {/* <ul className="app__navbar-links">
+                {["Home", "About", "Skills", "Portfolio", "Contact"].map((element) => (
+                    <li key={element}>
+                        <a href={`#${element}`}>{element}</a>
+                    </li>
+                ))}
+            </ul> */}
+            {/*We need to account for mobile devices by having a hamburger menu which pops out when we click on it at smaller screen widths. We will use Framer Motion for this*/}
+            <div className="app__navbar-menu">
+                <HiMenuAlt4 onClick={() => setToggle(true)}/>
 
-        </div>
-    </nav>
-    
-  )
+                {/*If toggle is true, then we want the menu to pop out. It will be within a motion.div */}
+
+                {toggle && (
+                    <motion.div
+                    whileInView={{x: [300, 0]}}
+                    transition={{duration: 0.85, ease: "easeOut"}}>
+                        {/*The "X" icon on the menu*/}
+                        <HiX onClick={() => setToggle(false)}/>
+                        <ul>                                          
+                            {["Home", "About", "Skills", "Portfolio", "Contact"].map((element) => (
+                            <li key={element}>
+                                <div />
+                                <a href={`#${element}`} >{element}</a>
+                            </li>
+                            ))}
+                        </ul>
+
+                    </motion.div>
+                )}
+
+
+            </div>
+        </nav>
+        
+    )
 };
 
 export default Navbar;
